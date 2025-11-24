0c19ab98b9a6aa2b45f7490106663538a10f5b9a
rbraju3m <rbraju3m@gmail.com>
2025-11-24 18:31:18 +0600

    create models & migrations

--- djshop/catalog/models.py
+++ djshop/catalog/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'categories'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, blank=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d/', blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ('category', 'slug')
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            # simple slug generation
+            from django.utils.text import slugify
+            base = slugify(self.name)
+            slug_candidate = base
+            n = 1
+            while Product.objects.filter(category=self.category, slug=slug_candidate).exclude(pk=self.pk).exists():
+                slug_candidate = f"{base}-{n}"
+                n += 1
+            self.slug = slug_candidate
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
